cdad40cc6454dcfc268e3920a1563959a32b21d2
sachin746 <sachinbhadur9@gmail.com>
2025-11-22 19:18:22 +0530

    aut

--- resources/sql/auth.sql
+++ resources/sql/auth.sql
@@ -1,9 +1,17 @@
-CREATE TABLE auth (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    password_hash TEXT,
-    google_id VARCHAR(100) UNIQUE,
-    email VARCHAR(100),
-    github_id VARCHAR(100) UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+create table public.auth (
+  password_hash text null,
+  google_id character varying(100) null,
+  email character varying(100) null,
+  created_at timestamp without time zone null default CURRENT_TIMESTAMP,
+  updated_at timestamp without time zone null default CURRENT_TIMESTAMP,
+  github_id character varying null,
+  id uuid not null,
+  constraint auth_pkey primary key (id),
+  constraint auth_google_id_key unique (google_id),
+  constraint auth_id_key unique (id),
+  constraint auth_id_fkey foreign KEY (id) references users (id)
+) TABLESPACE pg_default;
+
+create trigger set_updated_at BEFORE
+update on auth for EACH row
+execute FUNCTION update_updated_at_column ();
