8fa6e9f40135d71d3fb30836b6079863e41e7721
sachin746 <sachinbhadur9@gmail.com>
2025-11-22 19:29:39 +0530

    fix auth

--- entities/auth.go
+++ entities/auth.go
@@ -11,7 +11,7 @@ type Auth struct {
 	PasswordHash *string   `gorm:"type:text"`                                // nullable TEXT
 	GoogleID     *string   `gorm:"column:google_id;type:varchar(100)"`       // nullable, unique
 	Email        *string   `gorm:"type:varchar(100)"`                        // nullable
-	GithubID     *string   `gorm:"column:github_id;type:varchar(100)"`       // nullable, unique
+	GithubID     *string   `gorm:"column:github_id;type:varchar"`            // nullable
 	CreatedAt    time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"` // timestamp default current
 	UpdatedAt    time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"` // timestamp default current
 }
--- models/auth.go
+++ models/auth.go
@@ -9,7 +9,7 @@ import (
 type Auth struct {
 	ID           uuid.UUID `gorm:"primaryKey;not null"`                      // id SMALLINT PRIMARY KEY
 	PasswordHash *string   `gorm:"type:text"`                                // nullable TEXT
-	GoogleID     *string   `gorm:"type:varchar(100);uniqueIndex"`            // nullable, unique
+	GoogleID     *string   `gorm:"type:varchar(100);"`                       // nullable, unique
 	GoogleEmail  *string   `gorm:"type:varchar(100)"`                        // nullable
 	CreatedAt    time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"` // timestamp default current
 	UpdatedAt    time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"` // timestamp default current
